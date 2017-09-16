141da54702bbd82394ab55a068b6436bfef331b9
gaowenhu <gaowenhu@v-secure.cn>
2017-09-16 08:57:30 +0800

    201709160857

--- EngRefactor.py
+++ EngRefactor.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 '''
-	目前版本v0.6
+	目前版本v0.8--Refactor
 	
 	计划:
-	v0.7 --随机好多废数据.
 	v0.8 --用vb二次加密.
 	v0.9 --自变形.
 	v1.0 --发布.
@@ -27,6 +26,9 @@ varStr 	= []
 varStrRemoveRepeat = []
 varStrRemoveRepeat.append('ahoo')
 
+'''是不是需要生成几个全局类对象。'''
+fpClass = FileRead2Write()
+randCla = RandomSequence()
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8') #改变标准输出的默认编码 
 
@@ -87,28 +89,26 @@ class FileRead2Write(object):
 		fpW.close()
 		return True
 		
-	def OpenOutPath(self,OutPath, = '"D:\\Program Files (x86)\\Notepad++\\notepad++.exe" '):
+	def OpenOutPath(self,OutPath,program = '"D:\\Program Files (x86)\\Notepad++\\notepad++.exe" '):
 		return os.system(program + OutPath) 		# program = 'notepad.exe' 
 		
-	'''	list排序
+	'''	1.list排序
 		ransomFamilyList = list(set(ransomFamilyList))
-		list最后一行去掉\n
+		2.list最后一行去掉\n
 		ransomFamilyList[-1] = ransomFamilyList[-1].strip('\n')
-		去空格.
+		3.去空格.
 		ransomFamilyList.append(i.strip(' ') + '\n')	
 		
-		遍历目录
+		4.遍历目录
 		for parent,dirnames,filenames in os.walk(InputDir):
 			for filename in filenames:
 				fpRan.ReadInputFile(os.path.join(parent, filename)) 
 		
-		
+		5.for循环 #a =1
+		#[i for i in range(0,10,1)	a = a+i ]
 	'''
 		
-		
-		
-		
-		
+
 class RandomSequence(object):
 	def __init__(self):
 		pass
@@ -130,7 +130,6 @@ class RandomSequence(object):
 			return 'ahoo'
 			pass
 		
-		
 	def randStr_AZUp(self,minNum=2,maxNum=5):
 		randstr = ''
 		if minNum > maxNum:
@@ -142,8 +141,8 @@ class RandomSequence(object):
 			randstr = randstr + chrTem
 		return randstr
 	
-	#应该从正常的代码列表中随机一句,当废话用,混淆效果更好.
-	#吧代码生成一个pprint py库在这调用,下版.
+	#从正常的代码列表中随机一句,当废话用,混淆效果更好.
+	'''吧代码生成一个pprint py库在这调用,下版.'''
 	JsCodeList = ['new Function("a", "b", "return a+b;");','var ybdetof5 = new ActiveXObject("Scripting.FileSystemObject");']
 	def randCodeLine(self,CodeList = []):
 		if len(CodeList) == 0:
@@ -153,11 +152,11 @@ class RandomSequence(object):
 	def randChr(self):
 		return random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()')
 		
-	#在一段字符串中随选择若干个字符，形成列表
+	#随机字符list:['n','f','y','b']
 	def randChrEx_List(self):
 		return random.sample('zyxwvutsrqponmlkjihgfedcba', 5)
 		
-	#随机生成一个List
+	#随机生成一个List:['nihao','buhao','a']
 	def randStrList(self,minNum=3,maxNum = 5):
 		if minNum > maxNum:
 			minNum = min(minNum,maxNum)
@@ -166,25 +165,54 @@ class RandomSequence(object):
 		arrLengt= random.randint(minNum,maxNum)
 		arrList =[]
 		for i in range(0,arrLengt,1):
-			arrList.append(self.randStr_az())
+			arrList.append(self.randStr_az()+'\n')
 		return arrList
 	
-	#生成数组模式1,2直接替换为变量.
-	
+	#生成数组模式1:['nihao','buhao','a'][1]
+	def randStrArrary(self,itemstr):
+		arrList 	= self.randStrList()
+		index 		= random.randint(0,len(arrList)-1)
+		arrList[index] = itemstr
+		return str(arrList) + '[' + str(index) + ']'
+	#生成数组模式2: var ax = "buhao"  ---> ['nihao',ax,'a'][1]
+	def randStrArryEx_var(self,itemstr):
+		arrList 	= self.randStrList()
+		index 		= random.randint(0,len(arrList)-1)
+		
+		#随机一个varName,确保不要重复.
+		global varStrRemoveRepeat
+		varName = self.randStr_az(3,7)
+		while varName in varStrRemoveRepeat:
+			varName = randCla.randStr(4,8)
+		varStrItem = 'var '+ varName + ' = "' + listItem + '"'
+		varStrRemoveRepeat.append(varName)
+		
+		#生成数组
+		arrList[index] = varName
+		replaceTemp = str(arrList) + '[' + str(index) + ']'
+		
+		#替换为变量形式.
+		replaceTemp_pattern = re.compile('\''+varName+'\'')
+		replaceTemp = replaceTemp_pattern.sub(varName,replaceTemp)
+		
+		varStr.append(varStrItem)
+		
+		
+		return replaceTemp
 	
 	
 	#随机一个function vbs的
 	def randFun_Vb(self):
-		return 'waitfortest\r\n coming~'
+		return 'waitfortest\r\n coming~\r\n'
 		pass
 		
 	def randFunList_Vb(self,MaxLen):
 		funList=[]
-		[i for i in range(0,MaxLen,1) funList.append(self.randFun_Vb()) ]
+		for i in range(0,MaxLen,1):
+			funList.append(self.randFun_Vb())
 		return funList
 			
-		#a =1
-		#[i for i in range(0,10,1)	a = a+i ]
+		
 
 class StrSplitEng(object):
 
@@ -239,19 +267,87 @@ class StrSplitEng(object):
 		#print(result)
 		return result
 	
+class ReplaceEng(object):
+	
+	def __init__(self):
+		self.Name = 'ahoo'
+	
+	
+	#替换某一个元素为数组类型,同时生成变量列表.	ax = ['nihao',ax,'a'][1]
+	def replaceListItemUsArrary(self,listItem):
+		
+		#对分割后数组的某个元素进行替换:随机数组长度.(index = random -1)\
+		global randCla
+		
+		#random one list like this :['nihao','buhao','a']
+		arrList 	= randCla.randStrList()
+		arrLengt 	= len(arrList) 
+		index 		= random.randint(0,arrLengt-1)
+		#v0.6
+		varName = randCla.randStr(3,7)
+		while varName in varStrRemoveRepeat:
+			varName = randCla.randStr(4,8)
+			
+		varStrItem = 'var '+ varName + ' = "' + listItem + '"'
+		#-------------------------------
+		arrList[index] = varName
+		replaceTemp = str(arrList) + '[' + str(index) + ']'
+		
+		replaceTemp_pattern = re.compile('\''+varName+'\'')
+		replaceTemp = replaceTemp_pattern.sub(varName,replaceTemp)
+		
+		print(varStrItem)
+		print(replaceTemp)
 
+		return replaceTemp
+	
+	#替换符合条件的查找到的为数组类型.------------?应该在这个地方生成var同时返回
+	def replaceFindStrUsArrlist(self,findstr):
+		'''
+		list_split_line = []
+		list_split_line =  StrSplitLine(findstr)
+		print(list_split_line)
+		
+		
+		list_replace_usarry = []
+		for i_list_split_line in list_split_line:
+			strArry =  replaceListItemUsArrary(i_list_split_line)
+			list_replace_usarry.append(strArry)
+		print(list_replace_usarry)
+		'''
+		splitCla = StrSplitEng()
+		replacestr = ''
+		#少于3个字符怎么办.
+		if len(findstr) <=3:
+			return findstr
+		for i_list_split_line in splitCla.StrSplitLine(findstr):
+			replacestr = replacestr + replaceListItemUsArrary(i_list_split_line) + ' + '
+		#print(replacestr[:-3])
+		return replacestr[:-3]
+
+
+	
+	
+	
 		
 		
 #分割引擎
 def JSVirus_Split():
 	
-	fpClass = FileRead2Write(PutPath,OutPath)
+	global fpClass
+	global randCla
+
 	#1.读取文件到LineList
-	myInputList = fpClass.ReadInputFile()
+	global myInputList
+	myInputList = fpClass.ReadInputFile(PutPath)
+	'''
+	print(randCla.randChrEx_List())
+	fpClass.WriteOutputFileEx_LineStr(OutPath,"This is my first refactor code!")
+	fpClass.WriteOutputFile(OutPath,randCla.randFunList_Vb(8))
+	fpClass.WriteOutputFile(OutPath,randCla.randStrList(6,10))
+	fpClass.WriteOutputFileEx_ListShuffle(OutPath,myInputList)
+	'''
 	
-	fpClass.WriteOutputFileEx_LineStr("This is my first refactor code!")
-	fpClass.WriteOutputFile(myInputList)
-	fpClass.WriteOutputFileEx_ListShuffle(myInputList)
 	
 	'''
 	#2.1替换[] 和()之间的.
@@ -272,7 +368,7 @@ def JSVirus_Split():
 				line = patternFang.sub(replaceTempstr,line,count=1)
 			writeList.append(line)	
 			pass
-			
+	'''		
 	#2.2替换""之间的.	
 	writeList2 = []
 	for lin1 in myInputList:
@@ -292,72 +388,14 @@ def JSVirus_Split():
 					lin1 = m
 			writeList2.append(lin1)
 			pass
-	#print(writeList2)
-	'''
-	os.system('"D:\\Program Files (x86)\\Notepad++\\notepad++.exe" ' + OutPath)
+	fpClass.WriteOutputFile(OutPath,writeList2)
+	
+	
+	fpClass.OpenOutPath(OutPath)
 	print('The Code has been Splited,there is my advice! Thanks!')
 	return True	
 
 
-#替换符合条件的查找到的为数组类型.------------?应该在这个地方生成var同时返回
-def replaceFindStrUsArrlist(findstr):
-	'''
-	list_split_line = []
-	list_split_line =  StrSplitLine(findstr)
-	print(list_split_line)
-	
-	
-	list_replace_usarry = []
-	for i_list_split_line in list_split_line:
-		strArry =  replaceListItemUsArrary(i_list_split_line)
-		list_replace_usarry.append(strArry)
-	print(list_replace_usarry)
-	'''
-	replacestr = ''
-	#缺少个判断小于3个字符怎么办.
-	if len(findstr) <=3:
-		return findstr
-	for i_list_split_line in StrSplitLine(findstr):
-		replacestr = replacestr + replaceListItemUsArrary(i_list_split_line) + ' + '
-	#print(replacestr[:-3])
-	return replacestr[:-3]
-
-
-#替换某一个元素为数组类型	
-def replaceListItemUsArrary(listItem):
-	
-	#对分割后数组的某个元素进行替换:随机数组长度.(index = random -1)
-	arrLengt = 10
-	index 	= random.randint(0,arrLengt-1)
-	
-	
-		
-	#v0.6
-	varName = randStr(3,7)
-	while varName in varStrRemoveRepeat:
-		varName = randStr(4,8)
-		
-	varStrItem = 'var '+ varName + ' = "' + listItem + '"'
-	#-------------------------------
-	arrList[index] = varName
-	replaceTemp = str(arrList) + '[' + str(index) + ']'
-	
-	replaceTemp_pattern = re.compile('\''+varName+'\'')
-	replaceTemp = replaceTemp_pattern.sub(varName,replaceTemp)
-	
-	
-	
-	#print(arrList)
-	#print(listItem)
-	varStr.append(varStrItem)
-	varStrRemoveRepeat.append(varName)
-	print(varStrItem)
-	print(replaceTemp)
-
-	return replaceTemp
-
-
-#v0.8---先不用开发了,有时间做个重构和优化吧.
 	
 
 
